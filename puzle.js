0582d3ae327312021c054b7604679717211d3079
bassii <bachirsalehsahara@gmail.com>
2026-03-10 11:24:05 +0100

    puzle js
    recemos que funcione porque no he probado esto aun

--- puzle.js
+++ puzle.js
@@ -13,6 +13,23 @@
             hueco.addEventListener('dragover', (e) => {
                 e.preventDefuault();
             });
-        });
+        
         hueco.addEventListener('drop', (e) => {
             e.preventDefault();
+            const idPieza = piezaSeleccionada.getAttribute('data-id');
+            const idHueco = hueco.getAttribute('data-id');
+            if (idPieza === idHueco) {
+                hueco.innerHTML = "";
+                hueco.appendChild(piezaSeleccionada);
+                piezaSeleccionada.style.width= "100%";
+                piezaSeleccionada.style.height= "100%";
+                piezaSeleccionada.draggable= false;
+                piezaSeleccionada.classList.add('encajada');
+
+                victoria();
+            }else{
+                alert("esta pieza no va ahi");
+            }
+        });
+    });
+    
